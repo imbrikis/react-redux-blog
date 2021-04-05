75e2c621f129a694409e10d8e6c74fe563fd3c10
imbrikis <imbrikis@yahoo.com>
2021-04-05 15:34:29 -0600

    lesson 253

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import PostList from './PostList'
 
 const App = () => {
   return (
     <div className='ui container'>
-      <h1>Yup</h1>
+      <PostList />
     </div>
   )
 }
