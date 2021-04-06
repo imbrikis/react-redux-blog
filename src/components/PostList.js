976dcc1a876fc0b10131d2a2792ca25a49241737
imbrikis <imbrikis@yahoo.com>
2021-04-05 21:52:04 -0600

    wiring in thunk middleware

--- src/components/PostList.js
+++ src/components/PostList.js
@@ -1,7 +1,13 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { connect } from 'react-redux'
+import { fetchPosts } from '../actions'
 
-const PostList = () => {
+const PostList = ({ fetchPosts }) => {
+  useEffect(() => {
+    fetchPosts()
+  }, [])
+
   return <div>Post List</div>
 }
 
-export default PostList
+export default connect(null, { fetchPosts })(PostList)
